7ce77e9a53f2b2eb1efabfb7bc8bca5059d557ec
gaojm@huiyinxun.com <gaojm@huiyinxun.com>
2020-06-18 15:55:14 +0800

    first commit

---
+++ App/src/main/java/com/test/MainActivity.java
@@ -0,0 +1,22 @@
+package com.test;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * Author : gaojianming
+ * E-mail : gaojm@huiyinxun.com
+ * Date   : 2020/6/18 15:47
+ * Desc   :
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+    }
+
+}
